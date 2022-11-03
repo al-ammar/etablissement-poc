a643be5207cc4715ee5b82189db707c5fc2a3deb
A.ALAA <ammar.alaaeddine@gmail.com>
2022-11-03 17:26:03 +0100

    initial commit

---
+++ src/main/java/fr/reservations/chateau/ChateauApplication.java
@@ -0,0 +1,13 @@
+package fr.reservations.chateau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChateauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChateauApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/reservations/chateau/ChateauApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.reservations.chateau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChateauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
