fc423294ecb8a5b347d98cddce3b5a4b06b90e16
A.ALAA <ammar.alaaeddine@gmail.com>
2022-11-05 18:46:07 +0100

    changelog

--- chateau-common/src/main/java/fr/reservations/common/configuration/datasource/DataSourceConfiguration.java
+++ chateau-common/src/main/java/fr/reservations/common/configuration/datasource/DataSourceConfiguration.java
@@ -6,13 +6,34 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariDataSource;
 
-@EnableJpaRepositories("fr.reservations.persistence.repository")
-@EntityScan(basePackages = "fr.reservations.persistence.entity")
+//@EnableTransactionManagement
+@EnableJpaRepositories("fr.reservations.dao.repository")
+@EntityScan(basePackages = "fr.reservations.dao.entity")
 @Configuration
 public class DataSourceConfiguration {
+
+//	@Bean
+//	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+//		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+//		localContainerEntityManagerFactoryBean.setDataSource(dataSource());
+//		localContainerEntityManagerFactoryBean.setPersistenceUnitName("persitent");
+//		localContainerEntityManagerFactoryBean.setPackagesToScan("fr.reservations.persistence.entity");
+//		return localContainerEntityManagerFactoryBean;
+//	}
+//
+//	@Bean
+//	public PlatformTransactionManager transactionManager() {
+//		JpaTransactionManager transactionManager = new JpaTransactionManager();
+//		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+//		return transactionManager;
+//	}
 
 	@Bean
 	@ConfigurationProperties(prefix = "spring.datasource")
--- chateau-common/src/main/java/fr/reservations/common/configuration/services/ApplicationConfiguration.java
+++ chateau-common/src/main/java/fr/reservations/common/configuration/services/ApplicationConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
-@ComponentScan(value = { "ma.poc.services"})
+@ComponentScan(value = { "fr.reservations.services"})
 public class ApplicationConfiguration {
 
 	@Bean
